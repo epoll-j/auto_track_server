34b0276d12e321b87f339e32b4cb061aa32c3cf3
Dubhe <epoll@foxmail.com>
2024-03-13 22:33:40 +0800

    add: first commit

---
+++ src/api/config/config.default.ts
@@ -0,0 +1,30 @@
+export default {
+  koa: {
+    proxy: true
+  },
+  typeorm: {
+    dataSource: {
+      default: {
+        type: 'mariadb',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'track',
+        synchronize: false,
+        logging: false,
+        entities: [
+          '**/db_entity/*{.ts,.js}'
+        ]
+      }
+    }
+  },
+  redis: {
+    client: {
+      port: 6379,
+      host: "127.0.0.1", 
+      // password: "auth",
+      db: 0,
+    },
+  },
+}
---
+++ src/api/job/statistics.job.ts
@@ -0,0 +1,55 @@
+import { Job, IJob } from '@midwayjs/cron';
+import { FORMAT, Inject } from '@midwayjs/core';
+import { RedisService } from '@midwayjs/redis';
+import { InjectEntityModel } from '@midwayjs/typeorm';
+import { Repository } from 'typeorm';
+import { AppInfo } from '../db_entity/AppInfo';
+import { TrackStatistics } from '../db_entity/TrackStatistics';
+import { UserTrack } from '../db_entity/UserTrack';
+
+@Job({
+    cronTime: FORMAT.CRONTAB.EVERY_DAY,
+    start: true,
+})
+export class DataStatisticsJob implements IJob {
+
+    @Inject()
+    redis: RedisService
+    @InjectEntityModel(AppInfo)
+    appInfoModel: Repository<AppInfo>;
+    @InjectEntityModel(TrackStatistics)
+    statisticsModel: Repository<TrackStatistics>;
+    @InjectEntityModel(UserTrack)
+    userTrackModel: Repository<UserTrack>;
+
+    async onTick() {
+        const apps = await this.appInfoModel.findBy({
+            appStatus: 1
+        })
+
+        for (const app of apps) {
+            const appKey = app.appKey;
+            const dauKey = `${appKey}:dau`;
+            const nuKey = `${appKey}:nu`;
+
+            const dau = await this.redis.bitcount(dauKey) || 0;
+            const nu = await this.redis.get(nuKey) || 0;
+
+            const trackCount = await this.userTrackModel.createQueryBuilder('user_track')
+                .select('user_track.trackType', 'track_type')
+                .addSelect('COUNT(user_track.trackType)', 'num')
+                .where('DATE(user_track.createTime) = CURDATE()')
+                .groupBy('user_track.trackType')
+                .getRawMany();
+
+            const statistics = new TrackStatistics();
+            statistics.appKey = appKey;
+            statistics.appDau = Number(dau || 0);
+            statistics.appNu = Number(nu || 0);
+            statistics.otherParams = JSON.stringify(trackCount);
+            await this.statisticsModel.insert(statistics);
+            await this.redis.del(dauKey);
+            await this.redis.del(nuKey);
+        }
+    }
+}
