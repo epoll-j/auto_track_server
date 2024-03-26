d478458ac6e2e3d34d3437711cc9327fb71fdcb6
Dubhe <epoll@foxmail.com>
2024-03-26 16:35:10 +0800

    feat: add total user statistics

--- src/api/job/statistics.job.ts
+++ src/api/job/statistics.job.ts
@@ -7,6 +7,7 @@ import { AppInfo } from '../db_entity/AppInfo';
 import { TrackStatistics } from '../db_entity/TrackStatistics';
 import { UserTrack } from '../db_entity/UserTrack';
 import moment from 'moment';
+import { AppUser } from '../db_entity/AppUser';
 
 @Job({
     cronTime: FORMAT.CRONTAB.EVERY_DAY,
@@ -22,6 +23,8 @@ export class DataStatisticsJob implements IJob {
     statisticsModel: Repository<TrackStatistics>;
     @InjectEntityModel(UserTrack)
     userTrackModel: Repository<UserTrack>;
+    @InjectEntityModel(AppUser)
+    appUserModel: Repository<AppUser>;
 
     async onTick() {
         const apps = await this.appInfoModel.findBy({
@@ -35,18 +38,24 @@ export class DataStatisticsJob implements IJob {
 
             const dau = await this.redis.bitcount(dauKey) || 0;
             const nu = await this.redis.get(nuKey) || 0;
+            const tu = await this.appUserModel.countBy({
+                appKey
+            })
 
             const trackCount = await this.userTrackModel.createQueryBuilder('user_track')
-                .select('user_track.trackType', 'track_type')
-                .addSelect('COUNT(user_track.trackType)', 'num')
+                .select('user_track.trackKey', 'track_key')
+                .addSelect('user_track.trackType', 'track_type')
+                .addSelect('COUNT(user_track.id)', 'num')
                 .where('DATE(user_track.createTime) = DATE_SUB(CURDATE(), INTERVAL 1 DAY)')
-                .groupBy('user_track.trackType')
+                .groupBy('user_track.trackKey')
+                .addGroupBy('user_track.trackType')
                 .getRawMany();
 
             const statistics = new TrackStatistics();
             statistics.appKey = appKey;
             statistics.appDau = Number(dau || 0);
             statistics.appNu = Number(nu || 0);
+            statistics.appTu = Number(tu || 0);
             statistics.otherParams = JSON.stringify(trackCount);
             statistics.dataTime = moment().subtract(1, 'days').toDate();
             await this.statisticsModel.insert(statistics);
